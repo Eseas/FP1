43e07035c8bcbb58bc64cece1f9150d73108e91f
Kazimieras Senvaitis <kazimieras.senvaitis@gmail.com>
2017-10-08 09:27:28 +0300

    Parses moves recursively

--- Parser.hs
+++ Parser.hs
@@ -3,15 +3,14 @@ module Parser where
 data Point = Point (Integer, Integer)
   deriving Show
 
-data Move = A | Move { point  :: Point
-                 , symbol :: String
-                 , id     :: String
-                 , prev   :: Move
-                 } deriving Show
+data Move = Empty | Move { point  :: Point
+                         , symbol :: String
+                         , id     :: String
+                         , prev   :: Move
+                         } deriving Show
 
-
-msg :: String
-msg = "{\"c\": {\"0\": 1, \"1\": 2}, \"v\": \"x\", \"id\": \"QwjRrYJKKctJh\"}"
+m :: String
+m = "{\"c\": {\"0\": 1, \"1\": 1}, \"v\": \"x\", \"id\": \"Uw\", \"prev\": {\"c\": {\"0\": 1, \"1\": 2}, \"v\": \"x\", \"id\": \"Uw\"}}"
 
 parsePoint :: String -> (Point, String)
 parsePoint ('{':rest) =
@@ -36,30 +35,54 @@ parseString ('\"':rest) =
 parseString _ =
   error "String expected"
 
+-- indices = [[(x, y) | x <- [1..3]] | y <- [1..3]]
+--solve :: String -> xxx
+--solve rest =
+--  matrix = createMatrix move
+--  where
+--    (move, rest) = createMove rest
+--
+--
+--createMatrix :: Move -> [[Char]]
+--createMatrix move =
+--  Point (x, y) = point Move
+--
+--takePoint' ::
+
+createMove :: String -> (Move, String)
+createMove rest =
+  (move, rest1)
+  where
+    (move, rest1) = turboParse' rest (Move (Point (0,0)) "" "" Empty)
+
 turboParse' :: String -> Move -> (Move, String)
+-- init move
+turboParse' ('{':rest) (Move iniPoint ini1 ini2 prevMove) =
+  turboParse' rest newMove
+  where
+    newMove = Move iniPoint ini1 ini2 prevMove
 -- c - point
-turboParse' ('{':'\"':'c':'\"':':':' ':rest) (Move iniPoint ini1 ini2 A) =
+turboParse' ('\"':'c':'\"':':':' ':rest) (Move iniPoint ini1 ini2 prevMove) =
   turboParse' rest1 newMove
   where
     (point, rest1) = parsePoint rest
-    newMove = Move point "" "" A
+    newMove = Move point "" "" prevMove
 -- v - symbol
-turboParse' ('\"':'v':'\"':':':' ':rest) (Move iniPoint ini1 ini2 A) =
+turboParse' ('\"':'v':'\"':':':' ':rest) (Move iniPoint ini1 ini2 prevMove) =
   turboParse' rest1 newMove
   where
     (stringValue, rest1) = parseString rest
-    newMove = Move iniPoint stringValue "" A
+    newMove = Move iniPoint stringValue "" prevMove
 -- id
-turboParse' ('\"':'i':'d':'\"':':':' ':rest) (Move iniPoint ini1 ini2 A) =
+turboParse' ('\"':'i':'d':'\"':':':' ':rest) (Move iniPoint ini1 ini2 prevMove) =
   turboParse' rest1 newMove
   where
     (stringValue, rest1) = parseString rest
-    newMove = Move iniPoint ini1 stringValue A
+    newMove = Move iniPoint ini1 stringValue prevMove
 -- prev
-turboParse' ('\"':'i':'d':'\"':':':' ':rest) (Move iniPoint ini1 ini2 A) =
-  turboParse' rest1 newMove
+turboParse' ('\"':'p':'r':'e':'v':'\"':':':' ':rest) (Move iniPoint ini1 ini2 Empty) =
+  ((Move iniPoint ini1 ini2 newMove), rest1)
   where
-    (stringValue, rest1) = parseString rest
-    newMove = Move iniPoint ini1 stringValue A
-turboParse' rest (Move iniPoint ini1 ini2 A) =
-  ((Move iniPoint ini1 ini2 A), rest)
+    (newMove, rest1) = createMove rest
+turboParse' rest (Move iniPoint ini1 ini2 prevMove) =
+  ((Move iniPoint ini1 ini2 prevMove), rest)
