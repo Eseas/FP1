8193073cd700e332825dd2cd867143c0332424c0
Kazimieras Senvaitis <kazimieras.senvaitis@gmail.com>
2017-10-12 20:38:35 +0300

    Creating and filling the matrix.

--- Parser.hs
+++ Parser.hs
@@ -1,10 +1,14 @@
 module Parser where
+import Data.List        -- elemIndex
+import Data.Sequence    -- update
+import Data.Foldable    -- toList
+
 
 data Point = Point (Integer, Integer)
   deriving Show
 
 data Move = Empty | Move { point  :: Point
-                         , symbol :: String
+                         , symbol :: Char
                          , id     :: String
                          , prev   :: Move
                          } deriving Show
@@ -29,20 +33,44 @@ parseString :: String -> (String, String)
 parseString ('\"':rest) =
   let
       iAsStr = takeWhile (/= '\"') rest
-      strLenght = length iAsStr + 3
-      rest1 = drop strLenght rest
+      strLenght = Data.Foldable.length iAsStr + 3
+      rest1 = Data.List.drop strLenght rest
       in (iAsStr, rest1)
 parseString _ =
   error "String expected"
 
--- indices = [[(x, y) | x <- [1..3]] | y <- [1..3]]
---solve :: String -> xxx
---solve rest =
---  matrix = createMatrix move
---  where
---    (move, rest) = createMove rest
---
---
+indices = concat [[(x, y, ' ') | x <- [1..3]] | y <- [1..3]]
+
+fillMatrix :: Move -> [(Integer, Integer, Char)] -> [(Integer, Integer, Char)]
+fillMatrix (Move _ _ _ Parser.Empty) acc =
+--    putStrLn "last"
+    acc
+fillMatrix move acc =
+    fillMatrix previousTurn newAcc
+    where
+        Point (x, y) = point move
+        sym = symbol move
+        idx = case elemIndex (x, y, ' ') (takeWhile (<= (x, y, ' ')) acc) of
+            Just n  -> n
+            Nothing -> error "Could not extract index."
+        newAcc = toList(update idx (x, y, sym) $ fromList acc)
+        previousTurn = prev move
+--        putStrLn "full"
+
+
+
+
+solve :: String -> IO ()
+solve rest =
+    putStrLn "full"
+    where
+        (move, rest) = createMove rest
+        matrix = fillMatrix move indices
+
+test :: [(Integer, Integer, Char)]
+test =
+    [(1,2,'c'),(1,2,'d')]
+
 --createMatrix :: Move -> [[Char]]
 --createMatrix move =
 --  Point (x, y) = point Move
@@ -53,7 +81,7 @@ createMove :: String -> (Move, String)
 createMove rest =
   (move, rest1)
   where
-    (move, rest1) = turboParse' rest (Move (Point (0,0)) "" "" Empty)
+    (move, rest1) = turboParse' rest (Move (Point (0,0)) 'e' "" Parser.Empty)
 
 turboParse' :: String -> Move -> (Move, String)
 -- init move
@@ -66,13 +94,13 @@ turboParse' ('\"':'c':'\"':':':' ':rest) (Move iniPoint ini1 ini2 prevMove) =
   turboParse' rest1 newMove
   where
     (point, rest1) = parsePoint rest
-    newMove = Move point "" "" prevMove
+    newMove = Move point 'e' "" prevMove
 -- v - symbol
 turboParse' ('\"':'v':'\"':':':' ':rest) (Move iniPoint ini1 ini2 prevMove) =
   turboParse' rest1 newMove
   where
-    (stringValue, rest1) = parseString rest
-    newMove = Move iniPoint stringValue "" prevMove
+    ([charValue], rest1) = parseString rest
+    newMove = Move iniPoint charValue "" prevMove
 -- id
 turboParse' ('\"':'i':'d':'\"':':':' ':rest) (Move iniPoint ini1 ini2 prevMove) =
   turboParse' rest1 newMove
@@ -80,7 +108,7 @@ turboParse' ('\"':'i':'d':'\"':':':' ':rest) (Move iniPoint ini1 ini2 prevMove) 
     (stringValue, rest1) = parseString rest
     newMove = Move iniPoint ini1 stringValue prevMove
 -- prev
-turboParse' ('\"':'p':'r':'e':'v':'\"':':':' ':rest) (Move iniPoint ini1 ini2 Empty) =
+turboParse' ('\"':'p':'r':'e':'v':'\"':':':' ':rest) (Move iniPoint ini1 ini2 Parser.Empty) =
   ((Move iniPoint ini1 ini2 newMove), rest1)
   where
     (newMove, rest1) = createMove rest
