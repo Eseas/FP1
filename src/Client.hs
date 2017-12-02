6f0006a30d3cb739cb009e1fa406f69ee6b356ec
Kazimieras Senvaitis <kazimieras.senvaitis@erisata.lt>
2017-12-03 01:18:55 +0200

    Potentially working solving algorithm and Client returning correct string

--- src/Client.hs
+++ src/Client.hs
@@ -1,14 +1,48 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Client where
 
+import           Data.Aeson            (Value)
+import qualified Data.ByteString.Char8 as S8
 import qualified Data.ByteString.Lazy.Char8 as L8
+import qualified Data.Yaml             as Yaml
 import Network.HTTP.Simple
 
 makePostRequest :: IO ()
 makePostRequest = do
-  response <- httpLBS "http://httpbin.org/get"
+  --response <- httpLBS "http://httpbin.org/get"
+  --response <- httpLBS "http://tictactoe.haskell.lt/game/yolo80/player/2"
+
+  -- let request
+  --           = setRequestPath "/get"
+  --           $ setRequestHost "httpbin.org"
+  --           $ defaultRequest
+  -- response <- httpJSON request
+  --
+  -- putStrLn $ "The status code was: " ++
+  --            show (getResponseStatusCode response)
+  -- print $ getResponseHeader "Content-Type" response
+  -- S8.putStrLn $ Yaml.encode (getResponseBody response :: Value)
+
+-- good
+  let request
+            = setRequestPath "/game/yolo82/player/2"
+            $ setRequestHost "tictactoe.haskell.lt"
+            $ setRequestHeader "Accept" ["application/json+map"]
+            $ defaultRequest
+  response <- httpLBS request
 
   putStrLn $ "The status code was: " ++
              show (getResponseStatusCode response)
   print $ getResponseHeader "Content-Type" response
-  L8.putStrLn $ getResponseBody response
+  -- S8.putStrLn $ Yaml.encode (getResponseBody response :: Value)
+  L8.putStrLn (getResponseBody response)
+  --         -- $ setRequestHeader "Accept" ["application/json+map"]
+  --         -- $ setRequestPort 80
+  -- response <- httpJSON request
+  --
+  -- putStrLn $ "The status code was: " ++
+  --            show (getResponseStatusCode response)
+-- end good
+
+  --print $ getResponseHeader "Content-Type" response
+  --L8.putStrLn $ getResponseBody response
--- src/Solver.hs
+++ src/Solver.hs
@@ -31,3 +31,41 @@ makeAMove (elem:restElem) =
     case elem of
         (x, y, ' ') -> Just (x, y, 'x')
         _ -> makeAMove restElem
+
+makeAMove2 matrix (lx, ly, 'x')
+  case uzimtasOrOutside(lx + 1, ly + 2) of
+    Right (x, y, 'x') -> Just (x,y,'x')
+    Left e -> case checkIfFreeAndInside(lx + 2, ly + 1) of
+      Right (x, y, 'x') -> Just (x,y,'x')
+      Left e -> case uzimtasOrOutside(lx - 1, ly + 2) of
+              Right (x, y, 'x') -> Just (x,y,'x')
+              Left e -> case uzimtasOrOutside(lx - 2, ly + 1) of
+                      Right (x, y, 'x') -> Just (x,y,'x')
+                      Left e -> case uzimtasOrOutside(lx - 2, ly - 1) of
+                              Right (x, y, 'x') -> Just (x,y,'x')
+                              Left e -> case uzimtasOrOutside(lx - 1, ly - 2) of
+                                      Right (x, y, 'x') -> Just (x,y,'x')
+                                      Left e -> makeAMoveWhereNoLoss
+                                              no
+                                              yes -> "Javla"
+
+checkIfFreeAndInside :: [(Int, Int, Char)] -> Either String (Int, Int, Char)
+checkIfFreeAndInside matrix (x, y, sym)
+  | (x <= 2) && (x >= 0) && (y <= 2) && (y >= 0) = Left "Outside"
+  | otherwise = case position (x, y, sym) matrix
+                  -1 -> Left "Occupied"
+                  _ -> Right (x, y, sym)
+
+makeAMoveWhereNoLoss :: [(Int, Int, Char)] -> [(Int, Int, Char)] -> Maybe (Int, Int, Char)
+makeAMove [] matrix = Nothing
+makeAMoveWhereNoLoss (elem:restElem) matrix =
+    case elem of
+        (x, y, ' ') ->
+          case position (x, y, ' ') matrix of
+            -1 -> Nothing
+            idx ->
+              newAcc = changeElement idx (x,y, 'x') matrix
+              case checkGameOver matrix of
+                Right matrix -> Just (x, y, 'x')
+                Left e -> makeAMoveWhereNoLoss restElem matrix
+        (x, y, 'x') -> makeAMoveWhereNoLoss restElem matrix
